d82491621e7ba9edca91750b47d22359a75b0920
Adarsh Dwivedi <152579045+adarshh8@users.noreply.github.com>
2026-02-06 15:59:31 +0530

    Time: 3 ms (87.77%), Space: 43.1 MB (92.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char peek = st.peek();
+
+                if ((ch == ')' && peek == '(') ||
+                    (ch == ']' && peek == '[') ||
+                    (ch == '}' && peek == '{')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}

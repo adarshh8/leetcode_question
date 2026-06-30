9a2812d41ae70a200b474e6fa1832b9566b51f3f
Adarsh Dwivedi <152579045+adarshh8@users.noreply.github.com>
2026-06-30 14:55:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n==1){
-                return true;
+            return true;
         }
         if(n<=0){
             return false;
         }
-            while(n>1){
-                if(n%2!=0){
-                    return false;
-                }
-                n=n/2;
-            }
-        
-        return true;
+        if(n%2!=0){
+            return false;
+        }
+       return isPowerOfTwo(n/2);
     }
 };

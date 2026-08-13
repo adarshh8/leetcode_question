cbf015044c9509640b507ecf4b4fc3eda27b9aab
Adarsh Dwivedi <152579045+adarshh8@users.noreply.github.com>
2026-08-13 14:16:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (16.11%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i=0;i<s.length();i++){
-            // if(s[i]>='A' && s[i]<='Z')
-                // s[i]=s[i]+32;
+            if(s[i]>='A' && s[i]<='Z')
                 s[i]=tolower(s[i]);
         }
         return s;
